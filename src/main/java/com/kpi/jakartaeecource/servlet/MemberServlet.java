aaa0e4348489d5f1279a8881600d0f907b49c72b
dmytro.kovbasa <dmytro.kovbasa@pitchbook.com>
2025-02-09 02:09:21 +0200

    Lab1 | Added support DI

--- src/main/java/com/kpi/jakartaeecource/servlet/MemberServlet.java
+++ src/main/java/com/kpi/jakartaeecource/servlet/MemberServlet.java
@@ -3,6 +3,7 @@ package com.kpi.jakartaeecource.servlet;
 import com.kpi.jakartaeecource.dao.MemberDAO;
 import com.kpi.jakartaeecource.model.Member;
 import com.kpi.jakartaeecource.servlet.converter.MemberConverter;
+import jakarta.inject.Inject;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,22 +11,39 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 @WebServlet(name = "memberServlet", value = "/member")
 public class MemberServlet extends HttpServlet {
+    private static final Logger LOGGER = Logger.getLogger(MemberServlet.class.getName());
 
-    private final MemberDAO memberDAO = new MemberDAO();
+    @Inject
+    private  MemberDAO memberDAO;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        String memberId = request.getParameter("id");
-        int id = Integer.parseInt(memberId);
+        String memberIdParam = request.getParameter("id");
+        if (memberIdParam == null || memberIdParam.trim().isEmpty()) {
+            LOGGER.warning("Parameter 'id' is missing or empty.");
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing or empty parameter 'id'.");
+            return;
+        }
+
+        int id;
+        try {
+            id = Integer.parseInt(memberIdParam);
+        } catch (NumberFormatException e) {
+            LOGGER.info(String.format("Invalid format for parameter 'id': %s", memberIdParam));
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid format for parameter 'id'.");
+            return;
+        }
 
         Member member = memberDAO.getMemberById(id);
         if (member == null) {
-            response.sendError(HttpServletResponse.SC_NOT_FOUND, "Учасника з таким ID не знайдено");
+            LOGGER.info(String.format("Member with id %d not found.", id));
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, "Member with the specified ID not found.");
             return;
         }
 
