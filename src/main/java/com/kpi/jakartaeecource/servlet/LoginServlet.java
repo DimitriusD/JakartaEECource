d58be98da2a3ad76ae23d27ae7cd93696c0c3d8f
Dmytro Meheda <dmytro.meheda@contracted.pmi.com>
2025-02-10 16:18:45 +0200

    changed DI from field injection to constructor injection (where its possible)

--- src/main/java/com/kpi/jakartaeecource/servlet/LoginServlet.java
+++ src/main/java/com/kpi/jakartaeecource/servlet/LoginServlet.java
@@ -10,6 +10,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 
 @WebServlet(name = "loginServlet", value = "/login")
@@ -25,30 +26,29 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-      try {
-          String action = request.getParameter("action");
-          if ("loginUser".equals(action)) {
-              String username = request.getParameter("username");
-              String password = request.getParameter("password");
+        try {
+            String action = request.getParameter("action");
+            if ("loginUser".equals(action)) {
+                String username = request.getParameter("username");
+                String password = request.getParameter("password");
 
-              User user = userDAO.getByUserNameAndPass(username, password);
+                User user = userDAO.getByUserNameAndPass(username, password);
 
-              if (user.isAdmin()) {
-                  HttpSession session = request.getSession(true);
-                  session.setAttribute("role", "ADMIN");
-                  response.sendRedirect(request.getContextPath() + "/admin");
-              } else {
-                  request.getRequestDispatcher("/WEB-INF/views/currencies.jsp").forward(request, response);
-              }
-          } else if ("guest".equals(action)) {
-              response.sendRedirect(request.getContextPath() + "/currencies");
-          } else {
-              response.sendRedirect(request.getContextPath() + "/login");
-          }
-      } catch (Exception e) {
-          LoggerUtil.logError(getClass(), "Error processing login request", e);
-          response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "An internal error occurred. Please try again later.");
-      }
-
+                if (user.isAdmin()) {
+                    HttpSession session = request.getSession(true);
+                    session.setAttribute("role", "ADMIN");
+                    response.sendRedirect(request.getContextPath() + "/admin");
+                } else {
+                    request.getRequestDispatcher("/WEB-INF/views/currencies.jsp").forward(request, response);
+                }
+            } else if ("guest".equals(action)) {
+                response.sendRedirect(request.getContextPath() + "/currencies");
+            } else {
+                response.sendRedirect(request.getContextPath() + "/login");
+            }
+        } catch (Exception e) {
+            LoggerUtil.logError(getClass(), "Error processing login request", e);
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "An internal error occurred. Please try again later.");
+        }
     }
 }
